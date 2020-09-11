7c449de048604c7fedffeb5ebe98ff5544204901
Allen Wight <awight@redhat.com>
2020-09-11 10:17:54 -0400

    Hookup faq to api, update deps (#10)

--- src/Components/Tabs/FAQ.js
+++ src/Components/Tabs/FAQ.js
@@ -1,33 +1,32 @@
 import '../../App.scss';
 
-// import API from '../../Utilities/Api';
+import React, { useEffect, useState } from 'react';
+
+import API from '../../Utilities/Api';
 import { Card } from '@patternfly/react-core/dist/esm/components';
-import messages from '../../Messages';
-import React, { useState, useEffect } from 'react';
+import Loading from '../Loading/Loading';
 import ReactMarkdown from 'react-markdown/with-html';
-import { useIntl } from 'react-intl';
 
 const FAQ = () => {
-    const [content, setContent] = useState();
-    const intl = useIntl();
+    const [content, setContent] = useState({ status: 'pending', content: '' });
 
     useEffect(() => {
-        // Not currently working. Article is behind paywall and needs auth to access, or the auth removed.
-        // Hopefully hydra-dev team can help us out.
-        // (async() => {
-        //     const kcsArticleJson = (await API.get('https://access.redhat.com/hydra/rest/drupal/articles/4602981')).data;
-        //     setContent(kcsArticleJson.response.docs[0].body[0]);
-        // })();
-
-        // In the meantime, the content is hardcoded into the app
-        setContent(intl.formatMessage(messages.faqBody));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        (async () => {
+            try {
+                const kcsArticleJson = (await API.get('https://access.redhat.com/hydra/rest/drupal/articles/4602981')).data;
+                setContent({ status: 'fulfilled', content: kcsArticleJson.body.html });
+            } catch (error) {
+                setContent({ status: 'error', content: '' });
+                throw `${error}`;
+            }
+        })();
     }, []);
 
     return <div className='ins-c-flex-center'>
-        <Card className='faq-card'>
-            <ReactMarkdown source={content} escapeHtml={false} />
-        </Card>
+        {content.status === 'pending' || content.status === 'error' && <Loading />}
+        {content.status === 'fulfilled' && <Card className='faq-card'>
+            <ReactMarkdown source={content.content} escapeHtml={false} />
+        </Card>}
     </div>;
 };
 
