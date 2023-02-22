d077ecb4e0e0cbbf9aa4f6c1b39a7613befdef84
Ryan Deussing <ryandeussing@gmail.com>
2023-02-22 14:34:39 -0500

    use 2023 version in url

--- src/Components/Sections/DataProtection.js
+++ src/Components/Sections/DataProtection.js
@@ -6,67 +6,113 @@ import messages from '../../Messages';
 import { useIntl } from 'react-intl';
 
 const DataProtection = () => {
-    const intl = useIntl();
+  const intl = useIntl();
 
-    const protectionData = [{
-        collectionTitle: intl.formatMessage(messages.infrastructure),
-        collectionDesc: intl.formatMessage(messages.infrastructureBody),
-        cards: [{
-            title: intl.formatMessage(messages.openShiftDedicated),
-            body: intl.formatMessage(messages.openShiftDedicatedBody),
-            footer: link(intl.formatMessage(messages.openShiftDedicatedFooter), 'https://access.redhat.com/products/openshift-dedicated-red-hat')
-        }, {
-            title: intl.formatMessage(messages.cvePatching),
-            body: intl.formatMessage(messages.cvePatchingBody)
-        }, {
-            title: intl.formatMessage(messages.penetrationTesting),
-            body: intl.formatMessage(messages.penetrationTestingBody)
-        }, {
-            title: intl.formatMessage(messages.agileSoftware),
-            body: intl.formatMessage(messages.agileSoftwareBody)
-        },  {
-            title: intl.formatMessage(messages.restrictedData),
-            body: intl.formatMessage(messages.restrictedDataBody)
-        }]
-    }, {
-        collectionTitle: intl.formatMessage(messages.userAccess),
-        collectionDesc: intl.formatMessage(messages.userAccessBody),
-        cards: [{
-            title: intl.formatMessage(messages.redhatSso),
-            body: intl.formatMessage(messages.redhatSsoBody),
-            footer: link(intl.formatMessage(messages.redhatSsoFooter), 'https://access.redhat.com/start/how-to-create-and-manage-users')
-        }, {
-            title: intl.formatMessage(messages.roleBasedAccess),
-            body: intl.formatMessage(messages.roleBasedAccessBody),
-            footer: link(intl.formatMessage(messages.roleBasedAccessFooter), 'https://access.redhat.com/documentation/en-us/red_hat_insights/2020-10/html/user_access_configuration_guide_for_red_hat_insights/index')
-        }]
-    }, {
-        collectionTitle: intl.formatMessage(messages.dataRetention),
-        cards: [{
-            title: intl.formatMessage(messages.lastUploads),
-            body: intl.formatMessage(messages.lastUploadsBody)
-        }, {
-            title: intl.formatMessage(messages.automaticStale),
-            body: intl.formatMessage(messages.automaticStaleBody)
-        }]
-    }, {
-        collectionTitle: intl.formatMessage(messages.regulations),
-        collectionDesc: intl.formatMessage(messages.regulationsBody),
-        cards: [{
-            title: intl.formatMessage(messages.gdprPii),
-            // eslint-disable-next-line react/display-name
-            body: intl.formatMessage(messages.gdprPiiBody, { break: () => <React.Fragment><br /><br /></React.Fragment> }),
-            footer: link(intl.formatMessage(messages.gdprPiiFooter), 'https://www.redhat.com/en/gdpr')
-        }, {
-            title: intl.formatMessage(messages.dataControls),
-            body: intl.formatMessage(messages.dataControlsBody),
-            footer: link(intl.formatMessage(messages.dataControlsFooter), 'https://console.redhat.com/security/insights#section-data-collection')
-        }]
-    }];
+  const protectionData = [
+    {
+      collectionTitle: intl.formatMessage(messages.infrastructure),
+      collectionDesc: intl.formatMessage(messages.infrastructureBody),
+      cards: [
+        {
+          title: intl.formatMessage(messages.openShiftDedicated),
+          body: intl.formatMessage(messages.openShiftDedicatedBody),
+          footer: link(
+            intl.formatMessage(messages.openShiftDedicatedFooter),
+            'https://access.redhat.com/products/openshift-dedicated-red-hat'
+          ),
+        },
+        {
+          title: intl.formatMessage(messages.cvePatching),
+          body: intl.formatMessage(messages.cvePatchingBody),
+        },
+        {
+          title: intl.formatMessage(messages.penetrationTesting),
+          body: intl.formatMessage(messages.penetrationTestingBody),
+        },
+        {
+          title: intl.formatMessage(messages.agileSoftware),
+          body: intl.formatMessage(messages.agileSoftwareBody),
+        },
+        {
+          title: intl.formatMessage(messages.restrictedData),
+          body: intl.formatMessage(messages.restrictedDataBody),
+        },
+      ],
+    },
+    {
+      collectionTitle: intl.formatMessage(messages.userAccess),
+      collectionDesc: intl.formatMessage(messages.userAccessBody),
+      cards: [
+        {
+          title: intl.formatMessage(messages.redhatSso),
+          body: intl.formatMessage(messages.redhatSsoBody),
+          footer: link(
+            intl.formatMessage(messages.redhatSsoFooter),
+            'https://access.redhat.com/start/how-to-create-and-manage-users'
+          ),
+        },
+        {
+          title: intl.formatMessage(messages.roleBasedAccess),
+          body: intl.formatMessage(messages.roleBasedAccessBody),
+          footer: link(
+            intl.formatMessage(messages.roleBasedAccessFooter),
+            'https://access.redhat.com/documentation/en-us/red_hat_hybrid_cloud_console/2023/html/user_access_configuration_guide_for_role-based_access_control_rbac/index'
+          ),
+        },
+      ],
+    },
+    {
+      collectionTitle: intl.formatMessage(messages.dataRetention),
+      cards: [
+        {
+          title: intl.formatMessage(messages.lastUploads),
+          body: intl.formatMessage(messages.lastUploadsBody),
+        },
+        {
+          title: intl.formatMessage(messages.automaticStale),
+          body: intl.formatMessage(messages.automaticStaleBody),
+        },
+      ],
+    },
+    {
+      collectionTitle: intl.formatMessage(messages.regulations),
+      collectionDesc: intl.formatMessage(messages.regulationsBody),
+      cards: [
+        {
+          title: intl.formatMessage(messages.gdprPii),
+          // eslint-disable-next-line react/display-name
+          body: intl.formatMessage(messages.gdprPiiBody, {
+            break: () => (
+              <React.Fragment>
+                <br />
+                <br />
+              </React.Fragment>
+            ),
+          }),
+          footer: link(
+            intl.formatMessage(messages.gdprPiiFooter),
+            'https://www.redhat.com/en/gdpr'
+          ),
+        },
+        {
+          title: intl.formatMessage(messages.dataControls),
+          body: intl.formatMessage(messages.dataControlsBody),
+          footer: link(
+            intl.formatMessage(messages.dataControlsFooter),
+            'https://console.redhat.com/security/insights#section-data-collection'
+          ),
+        },
+      ],
+    },
+  ];
 
-    return <React.Fragment>
-        {protectionData.map(data => <CardCollection key={data.collectionTitle} {...data} />)}
-    </React.Fragment>;
+  return (
+    <React.Fragment>
+      {protectionData.map((data) => (
+        <CardCollection key={data.collectionTitle} {...data} />
+      ))}
+    </React.Fragment>
+  );
 };
 
 export default DataProtection;
